4b1cc75901cb22e3ddc71bdc61b27ba30e0c503d
Leonardo Alves <113029288+leoalvesrp@users.noreply.github.com>
2023-06-05 11:04:07 -0300

    transactions table

--- investimento/src/styles/pages/transactions/index.tsx
+++ investimento/src/styles/pages/transactions/index.tsx
@@ -1,11 +1,46 @@
 import { Header } from "../../../Components/Header";
 import { Sumary } from "../../../Components/Sumary";
+import { SearchForm } from "../Componentes/SearchForm";
+import { TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions(){
   return(
     <div>
       <Header/>
       <Sumary/>
+      <TransactionsContainer>
+        <SearchForm/>
+        <TransactionsTable>
+          <tbody>
+                <tr>
+                  <td width="40%">Tesouro direto</td>
+                  <td>
+                    <span>
+                      1200
+                    </span>
+                  </td>
+                  <td>Renda Fixa</td>
+                  <td>Compra</td>
+                  <td>
+                    05/06/2023
+                  </td>
+                </tr>
+                <tr>
+                  <td width="40%">bbsa3</td>
+                  <td>
+                    <span>
+                      800
+                    </span>
+                  </td>
+                  <td>Renda Variável</td>
+                  <td>Compra</td>
+                  <td>
+                    05/06/2023
+                  </td>
+                </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
 
     </div>
   )
